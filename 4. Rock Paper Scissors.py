6a24af1fd6fede01d75be8cd8c0e85a7b014f1f0
Vivek KUMAR <enigmaticvision@gmail.com>
2022-11-19 08:18:53 +0000

    First Commit

--- 4. Rock Paper Scissors.py
+++ 4. Rock Paper Scissors.py
@@ -1,20 +1,20 @@
 import random
 def play():
     user = input ("Choice rock [R], paper [P] or Scissors [S] :").lower()
-    computerChoice = random.choice(['r','p','s'])
-    print (f"Computer choice is {computerChoice}")
+    computer_choice = random.choice(['r','p','s'])
+    print (f"Computer choice is {computer_choice}")
 
-    if user==computerChoice:
+    if user==computer_choice:
         return "It is Tie !"
 
-    if is_Win(user,computerChoice):
+    if is_Win(user,computer_choice):
         return "Congratulations , You won and I lost"
 
     return "Computer won"
 
-def is_Win(user , computerChoice):
-    # r > s ; s > p ; p > r
-    if (user=='r' and computerChoice =='s' ) or (user=='s' and computerChoice =='p') or (user=='p' and computerChoice =='r'):
+def is_win(user, computer_choice):
+    # The rule of the game is , so when it follow this rule it return True r > s ; s > p ; p > r
+    if (user =='r' and computer_choice == 's') or (user == 's' and computer_choice == 'p') or (user == 'p' and computer_choice == 'r'):
         return True
 
 print (play())
